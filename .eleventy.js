de7731933399908e47d067ec6cf2b264dadf562d
Dinis <dinisareias@gmail.com>
2020-05-14 00:04:54 +0100

    add favicon and webmanifest

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
-module.exports = {
-  dir: {
-    input: "./",
-    output: "_site/"
+module.exports = function(eleventyConfig) {
+
+  // copy favicon files to site root
+  eleventyConfig.addPassthroughCopy({ "./favicon": "/" });
+
+  return {
+    dir: {
+      input: './',
+      output: './_site',
+    }
   }
-};
+}
