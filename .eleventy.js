db49cac60e4ff78e1c06768884233a1863f91f26
Dinis <dinisareias@gmail.com>
2020-05-14 00:20:48 +0100

    prepare deploy to github pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,16 @@
 module.exports = function(eleventyConfig) {
 
+  // site is not on root of domain. because github pages
+  const pathPrefix = "/irs-solidario/";
+
   // copy favicon files to site root
   eleventyConfig.addPassthroughCopy({ "./favicon": "/" });
 
   return {
     dir: {
       input: './',
-      output: './_site',
-    }
+      output: './_site'
+    },
+    pathPrefix: pathPrefix
   }
 }
