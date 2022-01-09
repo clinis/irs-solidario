463d1e7b56c1948406e72f24d66db1a1c07a3d95
Dinis <dinisareias@gmail.com>
2022-01-09 00:22:36 +0000

    Improve data parsing result

--- src/_data/xls.js
+++ src/_data/xls.js
@@ -1,10 +1,21 @@
 const xlsx = require('async-xlsx');
 
 module.exports = function() {
+  // Parse XLSX into JSON
   let xlsxJson = xlsx.parse(__dirname + '/Listagem_entidades_autorizadas_a_beneficiar_da_consignacao_2020.xlsx')
+
+  // Trim irrelevant data
   let parsedData = xlsxJson[0]['data']
   parsedData.splice(0, 4);  // strip title lines AND header line
+
+  // Return data as an array of ojects
   return {
-    data: parsedData,
+    data: parsedData.map(function(line) {
+      return {
+        "nome": line[1],
+        "nif": line[0],
+        "localidade": line[2]
+      }
+    })
   };
 }
