bc52cecfe522fc8a60061b1a1d448db2ec864c1f
Dinis <dinisareias@gmail.com>
2022-03-28 00:16:07 +0100

    Add cssnano to minify CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 const htmlnano = require('htmlnano')
-const CleanCSS = require('clean-css')
 
 module.exports = function(eleventyConfig) {
 
@@ -9,23 +8,24 @@ module.exports = function(eleventyConfig) {
   // copy favicon files to site root
   eleventyConfig.addPassthroughCopy({ "./src/favicon": "/" });
 
-  // copy Simple Datatables javascript files to site root
-  eleventyConfig.addPassthroughCopy({ "node_modules/simple-datatables/dist/umd/simple-datatables.js": "/simple-datatables-scripts.js" });
+  // copy Simple Datatables javascript file to site root
+  eleventyConfig.addPassthroughCopy({ "node_modules/simple-datatables/dist/umd/simple-datatables.js": "/simple-datatables-script.js" });
 
   // Set watch targets for CSS changes
   eleventyConfig.addWatchTarget('**/*.css')
 
-
-  /* Filter to minify CSS styles */
-  eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({ level: 2 }).minify(code).styles;
-  });
-
-  /* Transform to minify HTML */
+  /* Transform to minify HTML and CSS */
   eleventyConfig.addTransform("htmlmin", async function(content, outputPath) {
     if( outputPath.endsWith(".html") && process.env.ELEVENTY_ENV === 'production' ) {
       const options = {
-        collapseWhitespace: 'conservative'
+        collapseWhitespace: 'aggressive',
+        minifyCss: {
+          preset: ['default', {
+            discardComments: {
+              removeAll: true,
+            },
+          }]
+        }
       };
       const { html } = await htmlnano.process(content, options)
       return html
